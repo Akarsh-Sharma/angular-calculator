e6d4c6efa810c4aad1ccb5b250c52b87fa888477
Akarsh <akarsh311298@gmail.com>
2023-04-19 20:52:09 -0400

    Added onFunctionClick and onNumberClick methods.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-calculator';
+
+  calValue: number = 0; 
+  funcT : any = 'NoFunction';
+
+  calNumber : string = 'noValue';
+
+  firstNumber: number = 0;
+  secondNumber: number = 0;
+
+  onClickValue (val: string, type: any) {
+    if(type == 'number'){
+      this.onNumberClick(val);
+    } else if (type == 'function'){
+      this.onFunctionClick(val);
+    }
+  }
+
+  // if there is no initial value to read, set the current value to the entered val, 
+  // else add the new value to the display 
+  onNumberClick(val: string){
+    if(this.calNumber != 'noValue'){
+      this.calNumber = this.calNumber + val;
+    } else {
+      this.calNumber = val;
+    }
+
+    this.calValue = parseFloat(this.calNumber);
+  }
+
+  onFunctionClick(val: string){
+    if(this.funcT != 'NoFunction'){
+      this.firstNumber = this.calValue;
+      this.calValue = 0;
+      this.calNumber = 'noValue';
+      this.funcT = val;
+    }
+  }
 }
